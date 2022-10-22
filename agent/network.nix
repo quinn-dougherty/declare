bfc595fa540a248cbba263e0665c9d3b30694249
Quinn Dougherty <quinnd@riseup.net>
2022-10-21 23:42:21 -0400

    `nixfmt` compels meeeeeeeeeee

--- agent/network.nix
+++ agent/network.nix
@@ -2,9 +2,11 @@
   herc-agent = { modulesPath, lib, name, hercules-ci-agent, ... }: {
     imports =
       lib.optional (builtins.pathExists ./do-userdata.nix) ./do-userdata.nix
-      ++ [ (modulesPath + "/virtualisation/digital-ocean-config.nix") ]
+      ++ [
+        (modulesPath + "/virtualisation/digital-ocean-config.nix")
+      ]
       # ++ [ (import ./configuration.nix { inherit hercules-ci-agent; }) ];
-      ;
+    ;
     deployment = {
       targetHost = "64.225.11.209";
       targetUser = "root";
--- flake.nix
+++ flake.nix
@@ -77,11 +77,11 @@
               nix-doom-emacs hercules-ci-agent;
           };
         };
-       # "${agent.hostname}" = nixpkgs.lib.nixosSystem {
-       #   system = agent.system;
-       #   modules =
-       #     [ (import ./agent/network.nix { inherit hercules-ci-agent; }) ];
-       # };
+        # "${agent.hostname}" = nixpkgs.lib.nixosSystem {
+        #   system = agent.system;
+        #   modules =
+        #     [ (import ./agent/network.nix { inherit hercules-ci-agent; }) ];
+        # };
       };
 
       devShells.${framework.system}.home-development = framework.pkgs.mkShell {
@@ -112,28 +112,28 @@
         "${framework.hostname}-os".outputs =
           self.nixosConfigurations.${framework.hostname}.config.system.build.toplevel;
         dotfiles-lint.outputs = self.checks.${framework.system}.lint;
-       # agent-os = {
-       #   # outputs = self.nixosConfigurations.${agent.hostname}.config.system.build.toplevel;
-       #   outputs.effects = with agent.pkgs.effects;
-       #     runIf (src.ref == "refs/heads/main") (runNixOS {
-       #       configuration = ./agent/network.nix;
-       #
-       #       # this references secrets.json on your agent
-       #       secretsMap.ssh = "default-ssh";
-       #
-       #       # replace this with the appropriate line from ~/.ssh/known_hosts
-       #       userSetupScript = ''
-       #         writeSSHKey ssh
-       #         cat >>~/.ssh/known_hosts <<EOF
-       #         64.225.11.209 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMKFa4HNszRLl2G9m3+qkNDiQ3EPMZtdBlowBrb+jkfA
-       #         EOF
-       #       '';
-       #
-       #       # replace with hostname or ip address for ssh
-       #       ssh.destination = "64.225.11.209";
-       #
-       #     });
-       # };
+        # agent-os = {
+        #   # outputs = self.nixosConfigurations.${agent.hostname}.config.system.build.toplevel;
+        #   outputs.effects = with agent.pkgs.effects;
+        #     runIf (src.ref == "refs/heads/main") (runNixOS {
+        #       configuration = ./agent/network.nix;
+        #
+        #       # this references secrets.json on your agent
+        #       secretsMap.ssh = "default-ssh";
+        #
+        #       # replace this with the appropriate line from ~/.ssh/known_hosts
+        #       userSetupScript = ''
+        #         writeSSHKey ssh
+        #         cat >>~/.ssh/known_hosts <<EOF
+        #         64.225.11.209 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMKFa4HNszRLl2G9m3+qkNDiQ3EPMZtdBlowBrb+jkfA
+        #         EOF
+        #       '';
+        #
+        #       # replace with hostname or ip address for ssh
+        #       ssh.destination = "64.225.11.209";
+        #
+        #     });
+        # };
       };
     };
 }
