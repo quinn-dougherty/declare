c324dba9a3ae61cb9b594bb354aa42cfd65696cf
Quinn Dougherty <quinnd@riseup.net>
2022-10-24 23:08:50 -0400

    comment out `ssh.destination` in `effect.nix`

--- agent/effect.nix
+++ agent/effect.nix
@@ -1,22 +1,22 @@
 { ref, agent, hercules-ci-agent }:
 with agent.pkgs;
-effects.runIf (ref == "refs/heads/main") (effects.mkEffect {
+let herc-droplet-host = "64.225.11.209";
+in effects.runIf (ref == "refs/heads/main") (effects.mkEffect {
   effectScript = ''
     nix run ..#apps.nixinate.${agent.hostname}
   '';
 
   # this references secrets.json on your agent
-  secretsMap.ssh = "default-ssh";
-
+  secretsMap = { "ssh" = "default-ssh"; };
   # replace this with the appropriate line from ~/.ssh/known_hosts
   userSetupScript = ''
     writeSSHKey ssh
     cat >>~/.ssh/known_hosts <<EOF
-    64.225.11.209 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMKFa4HNszRLl2G9m3+qkNDiQ3EPMZtdBlowBrb+jkfA
+    ${herc-droplet-host} ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMKFa4HNszRLl2G9m3+qkNDiQ3EPMZtdBlowBrb+jkfA
     EOF
   '';
 
   # replace with hostname or ip address for ssh
-  ssh.destination = "64.225.11.209";
+  # ssh.destination = herc-droplet-host;
 
 })
