35b44e3402df906f44abad74bd000cc6bfbea251
Quinn Dougherty <quinnd@riseup.net>
2022-10-26 17:28:02 -0400

    fix `pubkeys-path`

--- agent/system/configuration.nix
+++ agent/system/configuration.nix
@@ -84,7 +84,7 @@
   # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users = let pubkeys-path = ./../common/pubkeys;
+  users.users = let pubkeys-path = ./../../common/pubkeys;
   in {
     ${agent.username} = {
       isNormalUser = true;
