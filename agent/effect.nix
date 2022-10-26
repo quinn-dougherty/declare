6dc32f99f5d68dde14475620797d7c16a61795de
Quinn Dougherty <quinnd@riseup.net>
2022-10-26 01:35:17 -0400

    try minor adjustment in `effect.nix`

--- agent/effect.nix
+++ agent/effect.nix
@@ -6,10 +6,10 @@ effects.runIf (ref == "refs/heads/main") (effects.mkEffect {
   '';
 
   # this references secrets.json on your agent
-  secretsMap = { "ssh" = "default-ssh"; };
+  secretsMap.ssh = "default-ssh";
   # replace this with the appropriate line from ~/.ssh/known_hosts
   userSetupScript = ''
-    writeSSHKey ssh
+    writeSSHKey ssh ~/.ssh/herc-default-id_rsa
     cat >>~/.ssh/known_hosts <<EOF
     ${agent.ip} ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINgl+QJ+NlZG22AASEkWHv+ihYdLzpLMUzbNG/MJb7ei
     EOF
