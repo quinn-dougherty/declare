e991631b4dc9b09c24d8ada584fc7c99a7d1a795
Quinn Dougherty <quinnd@riseup.net>
2022-10-26 19:59:20 -0400

    Automatic storage limits leading to garbage collection

--- agent/system/configuration.nix
+++ agent/system/configuration.nix
@@ -14,7 +14,11 @@
   ];
 
   nix = {
-    extraOptions = "experimental-features = nix-command flakes";
+    extraOptions = ''
+      experimental-features = nix-command flakes
+      min-free = ${toString (100 * 1024 * 1024)}
+      max-free = ${toString (1024 * 1024 * 1024)}
+    '';
     gc = {
       automatic = true;
       dates = "weekly";
