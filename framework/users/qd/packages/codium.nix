959323045e7a6cb4c9d8134fe913784d8d0e4c52
Quinn Dougherty <quinnd@riseup.net>
2022-10-26 20:52:07 -0400

    Bump `vscode-squiggle` to 0.5.0

--- framework/users/qd/packages/codium.nix
+++ framework/users/qd/packages/codium.nix
@@ -14,7 +14,7 @@
     ] ++ (pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
       name = "vscode-squiggle";
       publisher = "QURI";
-      version = "0.3.1";
-      sha256 = "113w2iis4zi4z3sqc3vd2apyrh52hbh2gvmxjr5yvjpmrsksclbd";
+      version = "0.5.0";
+      sha256 = "sha256-xkl6K2F3lc8bCds4pz/tHRiuAyk8l5HRfp9nxkUj/AI=";
     }]);
 }
