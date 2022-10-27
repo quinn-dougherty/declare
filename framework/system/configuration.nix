c5a26eb195385d3939f339c966046790b37a4c60
Quinn Dougherty <quinnd@riseup.net>
2022-10-26 23:21:00 -0400

    `security.audit.enable = true;`

--- framework/system/configuration.nix
+++ framework/system/configuration.nix
@@ -125,6 +125,8 @@ with framework; {
     };
   };
 
+  security.audit.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
