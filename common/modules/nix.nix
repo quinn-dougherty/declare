41cd7236e29889b1bfcec951c9a51bfe57963ebf
Quinn Dougherty <quinnd@riseup.net>
2023-07-21 15:33:38 -0400

    tweak garbage collector settings

--- common/modules/nix.nix
+++ common/modules/nix.nix
@@ -4,8 +4,8 @@
   nix = {
     extraOptions = ''
       experimental-features = nix-command flakes
-      min-free = ${toString (100 * 1024 * 1024)}
-      max-free = ${toString (1024 * 1024 * 1024)}
+      min-free = ${toString (2048 * 1024 * 1024)}
+      max-free = ${toString (16 * 1024 * 1024 * 1024)}
     '';
     settings.auto-optimise-store = true;
   };
