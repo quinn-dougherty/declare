3ab8cf0b7f89df2f3682d897fd89246e21a30120
Quinn Dougherty <quinnd@riseup.net>
2023-08-08 14:22:18 -0400

    Cleanup: `lint.nix` stylistic

--- common/lint.nix
+++ common/lint.nix
@@ -1,14 +1,15 @@
 { common }:
-common.pkgs.stdenv.mkDerivation {
+with common.pkgs;
+stdenv.mkDerivation {
   name = "dotfiles-lint";
   src = ./../.;
-  buildInputs = with common.pkgs; [ nixfmt nodePackages.prettier ];
+  buildInputs = [ nixfmt nodePackages.prettier ];
   buildPhase = ''
     for nixfile in $(find $src -type f | grep '[.]nix')
     do
       nixfmt --check $nixfile
     done
-    prettier --check $src
+    prettier --check $src --ignore-path .gitignore
   '';
   installPhase = "mkdir -p $out";
 }
