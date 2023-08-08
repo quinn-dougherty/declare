48da60881098c9d68cef274ca5ebd55f28efc866
Quinn Dougherty <quinnd@riseup.net>
2023-08-08 17:45:04 -0400

    rm `yarn2nix` again :/

--- framework/users/qd/packages/development/javascript.nix
+++ framework/users/qd/packages/development/javascript.nix
@@ -4,7 +4,7 @@ with pkgs; [
   nodePackages.typescript
   yarn
   node2nix
-  haskellPackages.yarn2nix
+  # haskellPackages.yarn2nix
   nodePackages.npm-check-updates
   nodePackages.prettier
   nodePackages.esy
