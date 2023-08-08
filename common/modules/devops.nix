9a3de49699b58a4bd35a6bec184dcf86b163c1a6
Quinn Dougherty <quinnd@riseup.net>
2023-08-08 02:11:12 -0400

    `nixops` can't be in `devops.nix`, for security, still. So it goes.

--- common/modules/devops.nix
+++ common/modules/devops.nix
@@ -2,7 +2,7 @@
 
 {
   environment.systemPackages = with pkgs; [
-    nixops # https://github.com/NixOS/nixops/issues/1521
+    # nixops # https://github.com/NixOS/nixops/issues/1521
     cachix
     # morph
     heroku
