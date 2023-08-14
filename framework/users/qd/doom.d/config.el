a564155109590c79e8a906d9b53d4911a88664e8
Quinn Dougherty <quinnd@riseup.net>
2023-08-14 12:59:01 -0400

    Rm `agda` config from doom build again. Add `tsm` to `battlenet` launch.

--- framework/users/qd/doom.d/config.el
+++ framework/users/qd/doom.d/config.el
@@ -68,13 +68,13 @@
   (setq lsp-haskell-formatting-provider "stylish-haskell"))
 (set-formatter! 'stylish-haskell "stylish-haskell" :modes '(haskell-mode ".hs"))
 ;; auto-load agda-mode for .agda and .lagda.md
-(setq auto-mode-alist
-   (append
-     '(("\\.agda\\'" . agda2-mode)
-       ("\\.lagda.md\\'" . agda2-mode))
-     auto-mode-alist))
- (load-file (let ((coding-system-for-read 'utf-8))
-                 (shell-command-to-string "agda-mode locate")))
+;; (setq auto-mode-alist
+;;   (append
+;;     '(("\\.agda\\'" . agda2-mode)
+;;       ("\\.lagda.md\\'" . agda2-mode))
+;;     auto-mode-alist))
+;; (load-file (let ((coding-system-for-read 'utf-8))
+;;                 (shell-command-to-string "agda-mode locate")))
 
 ;;
 ;; Here are some additional functions/macros that could help you configure Doom:
