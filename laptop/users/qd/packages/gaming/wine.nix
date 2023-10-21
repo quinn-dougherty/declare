c543cdbc0fb3e11e20a712e979121ad76ce81189
Quinn Dougherty <quinn@quinnd.net>
2023-10-21 12:39:38 -0700

    >>>>:] 1337 desktops, lmao

--- laptop/users/qd/packages/gaming/wine.nix
+++ laptop/users/qd/packages/gaming/wine.nix
@@ -1,7 +1,8 @@
 { pkgs }:
 with pkgs; [
+  wine # I think just for bootstrapping?
   lutris # Launcher
-  bottles # Alt launcher
+  # bottles # Alt launcher
   gamemode # Cpu optimizations
   protonup-qt # Manage wine packages to finetune lutris (gui)
   protonup-ng # Manage wine packages (cli)
