9f1c55c2872b4c981ab5435e74f0c68248f50a8b
Quinn Dougherty <quinn@quinnd.net>
2023-10-25 08:20:45 -0700

    better llm initialization-- added claude

--- laptop/system/services/x/xmonad.hs
+++ laptop/system/services/x/xmonad.hs
@@ -24,13 +24,14 @@ alt = mod1Mask
 keybinds = [ ((modShift, xK_z), spawn "xscreensaver-command -lock")
     , ((mod4Mask, xK_q), restart "xmonad" True)
     , ((modShift, xK_e), spawnOn "4" "emacs")
-    , ((modShift, xK_w), spawnOn "1" "firefox")
-    , ((modShift, xK_m), spawnOn "3" "thunderbird")
-    , ((modShift, xK_b), spawnOn "9" "lutris battlenet & lutris trade-skill-master")
-    , ((modShift, xK_s), spawnHere "flameshot launcher")
-    , ((modShift, xK_h), spawnHere "blueman-manager & pavucontrol")
-    , ((modShift, xK_g), spawnHere "firefox --new-window \"chat.openai.com/chat?model=gpt-4\"")
-    , ((modShift, xK_p), spawnOn "6" "bitwarden")
+    , ((modShift, xK_w), spawnOn "1" "firefox") -- w is for www
+    , ((modShift, xK_m), spawnOn "3" "thunderbird") -- m is for mail
+    , ((modShift, xK_b), spawnOn "9" "lutris battlenet & lutris trade-skill-master") -- b is for battlenet
+    , ((modShift, xK_s), spawnHere "flameshot launcher") -- s is for screenshot
+    , ((modShift, xK_h), spawnHere "blueman-manager & pavucontrol") -- h is for headphones
+    , ((modShift, xK_g), spawnHere "firefox --new-window \"chat.openai.com?model=gpt-4\"") -- g is for gpt
+    , ((modShift, xK_t), spawnHere "firefox --new-window \"claude.ai/chats\"") -- t is for talk
+    , ((modShift, xK_p), spawnOn "6" "bitwarden") -- p is for password
     , ((alt, xK_F1), spawn "wpctl set-mute @DEFAULT_SINK@ toggle")
     , ((alt, xK_F2), spawn "wpctl set-sink-volume @DEFAULT_SINK@ -10%")
     , ((alt, xK_F3), spawn "wpctl set-sink-volume @DEFAULT_SINK@ +10%")
