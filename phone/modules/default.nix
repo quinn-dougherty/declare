da4ced55737e96cd570ba985253e5298c83e745a
Quinn Dougherty <quinnd@riseup.net>
2023-10-31 16:56:15 -0700

    `mobile-nixos.lib` passed into plasma.nix module

--- phone/modules/default.nix
+++ phone/modules/default.nix
@@ -6,12 +6,17 @@
   ./../system/hardware-configuration.nix
   ./booter.nix
   home-manager.nixosModules.home-manager
-  (import ./plasma.nix { inherit mobile-nixos; })
+  (import ./plasma.nix {
+    inherit mobile-nixos;
+    config = phone.config;
+    lib = mobile-nixos.lib;
+    pkgs = phone.pkgs;
+  })
   # ./qt.nix
   ./../../common/modules/manyterms.nix
   ./../../common/modules/openssh.nix
   ./../../common/modules/nix.nix
   ./../../common/modules/bluetooth.nix
-  # ./../../common/modules/openssh.nix
+  ./../../common/modules/openssh.nix
   (import ./hm.nix { inherit phone; })
 ]
