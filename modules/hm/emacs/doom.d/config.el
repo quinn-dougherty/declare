2a3b404aea3e409075c91bab62cef78eddf19a17
Quinn Dougherty <quinnd@riseup.net>
2023-11-13 15:17:13 -0800

    added stream mode to doom fonts

--- modules/hm/emacs/doom.d/config.el
+++ modules/hm/emacs/doom.d/config.el
@@ -22,7 +22,8 @@
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 (setq doom-font (font-spec :family "monospace" :size 15 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "sans" :size 18))
+      doom-variable-pitch-font (font-spec :family "sans" :size 18)
+      doom-big-font (font-spec :family "monospace" :size 24))
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
