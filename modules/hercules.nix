9a67328cba7a303c92e8c32e446c06f4ad0544f3
Quinn Dougherty <quinnd@riseup.net>
2023-11-16 12:36:55 -0800

    init bincache

--- modules/hercules.nix
+++ modules/hercules.nix
@@ -1,13 +1,12 @@
 { config, lib, pkgs, ... }:
 
-# let secrets = import ./secrets; in
 {
   imports = [ ./openssh.nix ./secrets ];
   services.hercules-ci-agent = {
     enable = true;
     settings = {
       concurrentTasks = "auto";
-      staticSecretsDirectory = "/run/hercules-ci-agent-keys"; # config.secrix.hercules-ci-agent.secrets.dirName;
+      staticSecretsDirectory = "/run/hercules-ci-agent-keys";
     };
   };
   networking.firewall.allowedTCPPorts = [ 443 ];
--- modules/observability.nix
+++ modules/observability.nix
@@ -2,6 +2,7 @@
   environment.systemPackages = with pkgs; [
     nettools
     netproc
+    nmap
     lm_sensors
     xsensors
     inxi
