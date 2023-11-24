0c257f221c7347461c0440bc41b4a40daa3d3d6e
Quinn Dougherty <quinnd@riseup.net>
2023-11-24 12:16:15 -0800

    mv herc secrets to `secrets/herc` subdir

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,9 +1,9 @@
 {
   secrix.services = {
     hercules-ci-agent.secrets = {
-      "binary-caches.json".encrypted.file = ./binary-caches.json.age;
-      "secrets.json".encrypted.file = ./secrets.json.age;
-      "cluster-join-token.key".encrypted.file = ./cluster-join-token.key.age;
+      "binary-caches.json".encrypted.file = ./herc/binary-caches.json.age;
+      "secrets.json".encrypted.file = ./herc/secrets.json.age;
+      "cluster-join-token.key".encrypted.file = ./herc/cluster-join-token.key.age;
     };
     nextcloud.secrets = {
       "nextcloud-db-pass".encrypted.file = ./nextcloud-db-pass.age;
