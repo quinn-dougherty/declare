31b5aa7a73be5fa9c899af7c2199a134e0e2b6f0
Quinn Dougherty <quinnd@riseup.net>
2023-11-26 22:39:10 -0800

    even simpler nextcloud

--- modules/system/services/nextcloud/nextcloud.nix
+++ modules/system/services/nextcloud/nextcloud.nix
@@ -4,6 +4,7 @@
     package = pkgs.nextcloud27;
     configureRedis = true;
     hostName = "sync.quinn-dougherty.com";
+    https = true;
     config = {
       adminuser = "qd-admin";
       adminpassFile = "${pkgs.writeText "adminpass" "test123"}";
--- modules/system/services/nextcloud/nginx.nix
+++ modules/system/services/nextcloud/nginx.nix
@@ -11,18 +11,17 @@
 
     # Only allow PFS-enabled ciphers with AES256
     sslCiphers = "AES256+EECDH:AES256+EDH:!aNULL";
-    virtualHosts.${config.services.nextcloud.hostName} =
-      { # "sync.quinn-dougherty.com" = {
-        forceSSL = true;
-        enableACME = true;
-        listen = [{
-          addr = "127.0.0.1";
-          port = 8188;
-        }];
-      };
+    virtualHosts.${config.services.nextcloud.hostName} = {
+      forceSSL = true;
+      enableACME = true;
+      listen = [{
+        addr = "127.0.0.1";
+        port = 8188;
+      }];
+    };
   };
-  security.acme = {
-    acceptTerms = true;
-    defaults.email = "quinnd@riseup.net";
-  };
+  #security.acme = {
+  #  acceptTerms = true;
+  #  defaults.email = "quinnd@riseup.net";
+  #};
 }
