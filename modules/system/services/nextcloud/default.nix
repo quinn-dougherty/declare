a082e26b0e254d77b02fe5f1bb076ceb28f661db
Quinn Dougherty <quinnd@riseup.net>
2023-11-26 23:45:19 -0800

    nextcloud??

--- modules/system/services/nextcloud/default.nix
+++ modules/system/services/nextcloud/default.nix
@@ -8,14 +8,18 @@
       hostName = "sync.quinn-dougherty.com";
       https = true;
       config = {
+        dbtype = "pgsql";
         adminuser = "qd-admin";
         adminpassFile = "${pkgs.writeText "adminpass" "test123"}";
       };
       configureRedis = true;
       database.createLocally = true;
     };
-    nginx.virtualHosts.${config.services.nextcloud.hostName}.locations."/".proxyPass =
-      "http://127.0.0.1:80";
+    nginx.virtualHosts.${config.services.nextcloud.hostName} = {
+      locations."/".proxyPass = "http://127.0.0.1:8080";
+      forceSSL = true;
+      enableACME = true;
+    };
   };
   networking.firewall.allowedTCPPorts = [ 8080 80 ];
 }
