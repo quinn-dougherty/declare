b8aedb527401900d735046be2a98a8e612946415
Quinn Dougherty <quinnd@riseup.net>
2023-11-26 19:20:27 -0800

    correct https settings (really)

--- modules/system/nixserve.nix
+++ modules/system/nixserve.nix
@@ -5,7 +5,6 @@
     nix-serve = {
       enable = true;
       secretKeyFile = "/run/nix-serve-keys/cache-qd-priv-key.pem";
-      # config.secrix.services.nix-serve.secrets."cache-qd-priv-key.pem";
     };
     nginx = {
       enable = true;
@@ -19,6 +18,10 @@
         addSSL = true;
       };
     };
+  };
+  security.acme = {
+    acceptTerms = true;
+    certs."bincache.quinn-dougherty.com".email = "quinnd@riseup.net";
   };
   users = {
     groups.nix-serve = { };
