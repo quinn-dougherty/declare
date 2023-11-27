a781075cd3e584965abb3f116391dedf7fcc33d0
Quinn Dougherty <quinnd@riseup.net>
2023-11-26 19:16:18 -0800

    correct https settings

--- modules/system/nixserve.nix
+++ modules/system/nixserve.nix
@@ -10,13 +10,13 @@
     nginx = {
       enable = true;
       recommendedProxySettings = true;
-      virtualHosts = {
-        "bincache.quinn-dougherty.com" = {
-          locations."/".proxyPass =
-            "http://${config.services.nix-serve.bindAddress}:${
-              toString config.services.nix-serve.port
-            }";
-        };
+      virtualHosts."bincache.quinn-dougherty.com" = {
+        locations."/".proxyPass =
+          "http://${config.services.nix-serve.bindAddress}:${
+            toString config.services.nix-serve.port
+          }";
+        enableACME = true;
+        addSSL = true;
       };
     };
   };
@@ -25,13 +25,6 @@
     users.nix-serve = {
       group = "nix-serve";
       isSystemUser = true;
-    };
-  };
-  security.acme = {
-    acceptTerms = true;
-    certs."bincache.quinn-dougherty.com" = {
-      dnsProvider = "route53";
-      email = "quinnd@riseup.net";
     };
   };
 }
