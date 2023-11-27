870123c95b4e979fb39694a26557617bd5a6cf59
Quinn Dougherty <quinnd@riseup.net>
2023-11-27 08:43:19 -0800

    seafile rather than nextcloud

--- modules/system/services/seafile/default.nix
+++ modules/system/services/seafile/default.nix
@@ -4,11 +4,16 @@
   services = {
     seafile = {
       enable = true;
-      ccnetSettings = "http://sync.quinn-dougherty.com";
+      adminEmail = "quinnd@riseup.net";
+      initialAdminPassword = "test123";
+      ccnetSettings = {
+        General.SERVICE_URL = "http://sync.quinn-dougherty.com";
+      };
     };
     nginx.virtualHosts."sync.quinn-dougherty.com" = {
       addSSL = true;
       enableACME = true;
     };
   };
+  security.acme.certs."sync.quinn-dougherty.com".email = "quinnd@riseup.net";
 }
