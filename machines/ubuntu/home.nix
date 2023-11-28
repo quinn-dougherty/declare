53de7ad483955268febdfcf3ed1d7c147fadd0c5
Quinn Dougherty <quinnd@riseup.net>
2023-11-27 17:33:06 -0800

    improved home for ubuntu

--- machines/ubuntu/home.nix
+++ machines/ubuntu/home.nix
@@ -20,6 +20,25 @@ home-manager.lib.homeManagerConfiguration {
     {
       targets.genericLinux.enable = true;
       xdg.mime.enable = true;
+      programs.bash = {
+        enable = true;
+        profileExtra = ''
+          export XDG_DATA_DIRS=$HOME/.home-manager-share:$XDG_DATA_DIRS
+        '';
+      };
+
+      home.activation = {
+        linkDesktopApplications = {
+          after = [ "writeBoundary" "createXdgUserDirectories" ];
+          before = [ ];
+          data = ''
+            rm -rf $HOME/.home-manager-share
+            mkdir -p $HOME/.home-manager-share
+            cp -Lr --no-preserve=mode,ownership ${config.home.homeDirectory}/.nix-profile/share/* $HOME/.home-manager-share
+          '';
+        };
+      };
     }
+    { programs.fish.enable = true; }
   ];
 }
