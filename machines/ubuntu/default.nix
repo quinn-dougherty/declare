394035edafb9b95812dba5958a2e3552d52f39be
Quinn Dougherty <quinnd@riseup.net>
2023-11-29 16:35:53 -0800

    24.05 is SO BACK (in beta)

--- machines/ubuntu/default.nix
+++ machines/ubuntu/default.nix
@@ -1,7 +1,7 @@
 { lib, ubuntu, home-manager, nix-doom-emacs }:
 
 with ubuntu; {
-  inherit (ubuntu) system hostname drv-name-prefix;
+  inherit (ubuntu) system username hostname drv-name-prefix;
   homemanager = import ./home.nix { inherit ubuntu home-manager; };
   homeshell = { };
 }
