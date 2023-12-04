3a824cf5c03eca2b752bd527daa71d47218863f6
Quinn Dougherty <quinnd@riseup.net>
2023-12-03 18:38:08 -0800

    >:] rm `manegarm` from default themes

--- modules/emacs/doom.d/config.el
+++ modules/emacs/doom.d/config.el
@@ -27,7 +27,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq rand-theme-wanted '(doom-outrun-electric doom-acario-dark doom-manegarm manoj-dark))
+(setq rand-theme-wanted '(doom-outrun-electric doom-acario-dark manoj-dark)) ; doom-manegarm ))
 (rand-theme)
 
 ; other defaults for startup.
