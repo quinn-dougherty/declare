0d0aacced48d9d4fe0f85619472dfcf6bb329fab
Quinn Dougherty <quinnd@riseup.net>
2024-01-23 08:00:04 -0800

    rename secrets dir correctly

--- modules/system/services/hercules.nix
+++ modules/system/services/hercules.nix
@@ -3,8 +3,8 @@
 {
   imports = [ ./openssh.nix "${inputs.self}/secrets" ];
   services.hercules-ci-agents = {
-    default.settings.staticSecretsDirectory = "/run/hercules-ci-agent-default-keys";
-    casper.settings.staticSecretsDirectory = "/run/hercules-ci-agent-casper-keys";
+    default.settings.staticSecretsDirectory = "/run/hci-default-keys";
+    casper.settings.staticSecretsDirectory = "/run/hci-casper-keys";
   };
   networking.firewall.allowedTCPPorts = [ 443 ];
 }
