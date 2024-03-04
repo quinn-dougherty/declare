c54befea808c4b738845c1c48b90a4cd3979974f
Quinn Dougherty <quinnd@riseup.net>
2024-03-04 08:39:29 -0800

    improve formatting

--- modules/system/emacs/doom/init.el
+++ modules/system/emacs/doom/init.el
@@ -158,9 +158,9 @@
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
-                                        ; (ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-                                        ; have to temporarily disable rust
-                                        ; (rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;; (ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       ;;; have to temporarily disable rust
+       ;; (rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        (sh +bash +fish)               ; she sells {ba,z,fi}sh shells on the C xor
