f421ef25ad1d99d9e824e9d067b362d6e8c71e10
Quinn Dougherty <quinnd@riseup.net>
2024-03-12 11:44:11 -0700

    rm nginx from seafile module

--- modules/system/services/seafile/default.nix
+++ modules/system/services/seafile/default.nix
@@ -6,14 +6,14 @@
       enable = true;
       adminEmail = "quinnd@riseup.net";
       initialAdminPassword = "test123";
-      ccnetSettings = {
-        General.SERVICE_URL = "http://sync.quinn-dougherty.com";
-      };
+      # ccnetSettings = {
+      #   General.SERVICE_URL = "http://sync.quinn-dougherty.com";
+      # };
     };
-    nginx.virtualHosts."sync.quinn-dougherty.com" = {
-      addSSL = true;
-      enableACME = true;
-    };
+    # nginx.virtualHosts."sync.quinn-dougherty.com" = {
+    #   addSSL = true;
+    #   enableACME = true;
+    # };
   };
   security.acme.certs."sync.quinn-dougherty.com".email = "quinnd@riseup.net";
 }
