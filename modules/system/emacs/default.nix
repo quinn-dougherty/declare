eb4f21bbefcb1c27ff4e521236f668e4a0ea4b44
Quinn Dougherty <quinnd@riseup.net>
2024-06-30 14:47:09 -0700

    trying emacs30, `nix flake update` monthly, reinstall networkmanager on server

--- modules/system/emacs/default.nix
+++ modules/system/emacs/default.nix
@@ -10,7 +10,7 @@
 with lib;
 let
   cfg = config.editors.emacs;
-  emacsPackage = (pkgs.emacsPackagesFor pkgs.emacs).emacsWithPackages (
+  emacsPackage = (pkgs.emacsPackagesFor pkgs.emacs-unstable).emacsWithPackages (
     epkgs: with epkgs; [
       vterm
       sqlite3
