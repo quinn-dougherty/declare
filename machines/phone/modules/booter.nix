f9555b8e3850ae0c1fa72446f04ac06489ae37a0
Quinn Dougherty <quinnd@riseup.net>
2024-07-02 09:15:07 -0700

    rm `useStrictKernelConfig`

--- machines/phone/modules/booter.nix
+++ machines/phone/modules/booter.nix
@@ -8,6 +8,9 @@
 {
   mobile.boot.stage-1 = {
     networking.enable = lib.mkDefault true;
-    kernel.useStrictKernelConfig = lib.mkDefault true;
+   # kernel = {
+   #   useStrictKernelConfig = lib.mkDefault true;
+   #   package = pkgs.linuxPackages_6_9_hardened;
+   # }; };
   };
 }
