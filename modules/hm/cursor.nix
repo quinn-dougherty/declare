fc50b4a365f36863375b5065c691b9b4604363a4
Quinn Dougherty <quinnd@riseup.net>
2025-01-22 14:16:15 -0800

    Bump cursor to 0.44.11

--- modules/hm/cursor.nix
+++ modules/hm/cursor.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 
 let
-  url = "https://downloader.cursor.sh/linux/appImage/x64";
-  sha256 = "sha256-Qcph6UYR+VtlaYNqHJpIGEVHu1q8Vbfvm5Bv6swf+pM=";
+  url = "https://download.todesktop.com/230313mzl4w4u92/cursor-0.44.11-build-250103fqxdt5u9z-x86_64.AppImage";
+  sha256 = "sha256-eOZuofnpED9F6wic0S9m933Tb7Gq7cb/v0kRDltvFVg=";
   cursor = pkgs.appimageTools.wrapType2 {
     pname = "cursor";
     version = "0.44.11";
