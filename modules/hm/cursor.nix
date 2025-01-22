8fc9b73ed9eb427c2a65a979fd54f22c267b92f5
Quinn Dougherty <quinnd@riseup.net>
2025-01-22 13:51:07 -0800

    bump cursor to 44.11

--- modules/hm/cursor.nix
+++ modules/hm/cursor.nix
@@ -5,7 +5,7 @@ let
   sha256 = "sha256-Qcph6UYR+VtlaYNqHJpIGEVHu1q8Vbfvm5Bv6swf+pM=";
   cursor = pkgs.appimageTools.wrapType2 {
     pname = "cursor";
-    version = "0.35.0";
+    version = "0.44.11";
     src = pkgs.fetchurl { inherit url sha256; };
   };
 in
