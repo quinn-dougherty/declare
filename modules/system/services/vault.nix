4b868c0e468feab5cab7d504eb542515b8923a51
Quinn Dougherty <quinn.dougherty.forall@gmail.com>
2025-04-15 21:26:33 -0700

    Fix loopback

--- modules/system/services/vault.nix
+++ modules/system/services/vault.nix
@@ -10,7 +10,7 @@
     vaultwarden = {
       enable = true;
       config = {
-        ROCKET_ADDRESS = "::1";
+        ROCKET_ADDRESS = "127.0.0.1";
         ROCKET_PORT = 8222;
         DOMAIN = "https://vault.quinn-dougherty.com";
       };
