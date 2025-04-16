eb76047379d2e1d0b9835b937341aeba4a0ce5df
Quinn Dougherty <quinn.dougherty.forall@gmail.com>
2025-04-15 19:33:27 -0700

    fprintd

--- machines/laptop/modules.nix
+++ machines/laptop/modules.nix
@@ -17,6 +17,7 @@ with inputs;
   "${modpath}/bluetooth.nix"
   "${modpath}/crosscompilation.nix"
   "${modpath}/desktops/il8n.nix"
+  "${modpath}/services/fprintd.nix"
   inputs.nixos-generators.nixosModules.all-formats
   {
     imports = [ "${modpath}/emacs" ];
---
+++ modules/system/services/fprintd.nix
@@ -0,0 +1,5 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services.fprintd.enable = true;
+}
